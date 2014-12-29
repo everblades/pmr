0c7f7a22a52184fa17092c3024fe7e474739d606
everblades <jrmany@gmail.com>
2014-12-29 00:23:43 -0500

    Initial Commit

---
+++ app/models/post.rb
@@ -0,0 +1,6 @@
+class Post < ActiveRecord::Base
+  validates_presence_of :title, :body, :excerpt
+  belongs_to :user
+  has_attached_file :photo, styles: {:medium => "300x300>", :thumb => "100x100#"}
+  validates_attachment_content_type :photo, content_type: /\Aimage\/.*\Z/
+end
