c82e012c9747e87eb25b0d44e5b3e5894a951c60
Jeffrey Many <jrmany@gmail.com>
2015-04-19 10:01:58 -0400

    Redirect to domain from herokuapp

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,6 @@
 class ApplicationController < ActionController::Base
+  before_filter :redirect_to_domain if Rails.env.production?
+
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
@@ -10,4 +12,14 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:avatar, :name) }
   end
 
+  private
+
+    # Redirect to the appropriate domain i.e. example.com
+    def redirect_to_domain
+      domain_to_redirect_to = 'www.premedroadmap.com'
+      domain_exceptions = ['premedroadmap.com', 'www.premedroadmap.com']
+      should_redirect = !(domain_exceptions.include? request.host)
+      new_url = "#{request.protocol}#{domain_to_redirect_to}#{request.fullpath}"
+      redirect_to new_url, status: :moved_permanently if should_redirect
+    end
 end
